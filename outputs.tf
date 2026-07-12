41e9cdeb078d2d620aacc49f8bfc8196d1cd2e70
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "subscription_template_deployments" {
-  description = "All subscription_template_deployment resources"
-  value       = azurerm_subscription_template_deployment.subscription_template_deployments
-}
 output "subscription_template_deployments_debug_level" {
-  description = "List of debug_level values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.debug_level]
+  description = "Map of debug_level values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.debug_level }
 }
 output "subscription_template_deployments_location" {
-  description = "List of location values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.location]
+  description = "Map of location values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.location }
 }
 output "subscription_template_deployments_name" {
-  description = "List of name values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.name]
+  description = "Map of name values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.name }
 }
 output "subscription_template_deployments_output_content" {
-  description = "List of output_content values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.output_content]
+  description = "Map of output_content values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.output_content }
 }
 output "subscription_template_deployments_parameters_content" {
-  description = "List of parameters_content values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.parameters_content]
+  description = "Map of parameters_content values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.parameters_content }
 }
 output "subscription_template_deployments_tags" {
-  description = "List of tags values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.tags]
+  description = "Map of tags values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.tags }
 }
 output "subscription_template_deployments_template_content" {
-  description = "List of template_content values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.template_content]
+  description = "Map of template_content values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.template_content }
 }
 output "subscription_template_deployments_template_spec_version_id" {
-  description = "List of template_spec_version_id values across all subscription_template_deployments"
-  value       = [for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : v.template_spec_version_id]
+  description = "Map of template_spec_version_id values across all subscription_template_deployments, keyed the same as var.subscription_template_deployments"
+  value       = { for k, v in azurerm_subscription_template_deployment.subscription_template_deployments : k => v.template_spec_version_id }
 }
 
